eaba3195c808e175ac6324a509e683f4f8333936
Andrei_ert <andrei.kozitsky@mail.ru>
2018-12-29 19:50:53 +0200

    revert after hosting

--- src/main/java/com/kozitski/pufar/annotation/engine/di/engine/DITestServlet.java
+++ src/main/java/com/kozitski/pufar/annotation/engine/di/engine/DITestServlet.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+// REDUNDANT pakage
 @WebServlet("/di")
 public class DITestServlet extends InjectionServlet {
 
